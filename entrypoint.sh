cbde33c483209e1c9a8a059bf347a5ed02b23d76
Thomas Nicollet <thomas.nicollet@epitech.eu>
2021-09-03 00:56:40 +0200

    :bug: Fixed entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -n "$2" ] && [ -n "$3" ]; then
-	git config --global url."https://$GH_USER:$GH_PATH@github.com".insteadOf "https://github.com"
+	git config --global url."https://$1:$2@github.com".insteadOf "https://github.com"
 fi
 
 bash -c $1
