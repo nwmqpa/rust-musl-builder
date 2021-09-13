974ed82083b2d65ad9c45be9782ac1c88fb4f09c
Thomas Nicollet <thomas.nicollet@epitech.eu>
2021-09-13 11:20:19 +0200

    :sparkles: Added environnement variable for bash -c

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,9 @@ if [ -n "$2" ] && [ -n "$3" ]; then
 fi
 
 if [ -n "$4" ] && [ -n "$5" ] && [ -n "$6" ]; then
-	SCCACHE_BUCKET="$4"
-	AWS_ACCESS_KEY_ID="$5"
-	AWS_SECRET_ACCESS_KEY="$6"
+	export SCCACHE_BUCKET="$4"
+	export AWS_ACCESS_KEY_ID="$5"
+	export AWS_SECRET_ACCESS_KEY="$6"
 	mkdir -p $HOME/.cargo
 	echo -e "[build]\nrustc-wrapper = \"$(which sccache)\"\n" >> $HOME/.cargo/config
 fi
