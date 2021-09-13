998de7bdcdf2a7a903d23b48c4c59a0c8b3b9661
Thomas Nicollet <thomas.nicollet@epitech.eu>
2021-09-13 11:00:38 +0200

    :bug: Fixing repository unknown

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,7 @@ if [ -n "$4" ] && [ -n "$5" ] && [ -n "$6" ]; then
 	SCCACHE_BUCKET="$4"
 	AWS_ACCESS_KEY_ID="$5"
 	AWS_SECRET_ACCESS_KEY="$6"
+	mkdir -p $HOME/.cargo
 	echo -e "[build]\nrustc-wrapper = \"$(which sccache)\"\n" >> $HOME/.cargo/config
 fi
 
